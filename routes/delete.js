0af5d6bdcc781678461a3c14446cf954aec92c0e
Rafael Ojeda <rafael.e.ojeda@gmail.com>
2025-01-23 17:49:56 -0500

    [InitIgniteBackend #1] Added ability to delete your own account

--- routes/delete.js
+++ routes/delete.js
@@ -1,8 +1,25 @@
 import express from "express";
-import {deleteDoc, doc, setDoc} from "firebase/firestore";
-import {db} from "../firebase.js";
+import {auth, db, deleteUser} from "../firebase.js";
 
 const router = express.Router();
+
+router.post("/user/currentUser", async (req, res) => {
+    const currentUser = auth.currentUser;
+
+    if (!currentUser) {
+        return res.status(401).json({ message: "You are not logged in!" });
+    }
+
+    deleteUser(currentUser).then(() => {
+        console.log(`User: ${currentUser} deleted successfully.`);
+        return res.status(200).json({ message: `Deleted user ${currentUser} successfully.` });
+    }).catch((err) => {
+        console.log(`Could not delete user: ${currentUser}`);
+        return res.status(401).json({ message: `Unable to delete user: ${currentUser} Reason: ${err}` });
+    });
+
+
+})
 
 router.post("/semester/student", async (req, res) => {
     const { semester, student_id} = req.body;
