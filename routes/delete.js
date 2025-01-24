d1ec988c1dea697c9c97b598bc296d327bf5350f
Rafael Ojeda <rafael.e.ojeda@gmail.com>
2025-01-24 17:04:03 -0500

    [InitIgniteBackend #1] When user deletes account it's corresponding document within Users collection is deleted.

--- routes/delete.js
+++ routes/delete.js
@@ -1,24 +1,50 @@
 import express from "express";
-import {auth, db, deleteUser} from "../firebase.js";
+import {auth, db, deleteDoc, deleteUser, doc} from "../firebase.js";
 
 const router = express.Router();
 
 router.post("/user/currentUser", async (req, res) => {
     const currentUser = auth.currentUser;
-
+    const currentUserId = currentUser.uid;
     if (!currentUser) {
         return res.status(401).json({ message: "You are not logged in!" });
     }
 
     deleteUser(currentUser).then(() => {
-        console.log(`User: ${currentUser} deleted successfully.`);
-        return res.status(200).json({ message: `Deleted user ${currentUser} successfully.` });
+        console.log(
+            `User: ${currentUser} deleted from Authentication successfully.`
+        );
     }).catch((err) => {
-        console.log(`Could not delete user: ${currentUser}`);
-        return res.status(401).json({ message: `Unable to delete user: ${currentUser} Reason: ${err}` });
+        console.log(
+            `Could not delete user: ${currentUserId} from Authentication. Reason: ${err}`
+        );
+        return res.status(400).json(
+            {
+                message: `Unable to delete user: ${currentUserId} from Authentication. Reason: ${err}`
+            }
+            );
     });
+    console.log(currentUserId)
+    deleteDoc(doc(db, "Users", currentUserId)).then(() => {
+        console.log(
+            `User: ${currentUser} deleted from Users collection successfully.`
+        );
+    }).catch((err) => {
+        console.log(
+            `Could not delete user: ${currentUserId} from Users collection. Reason: ${err}`
+        );
+        return res.status(400).json(
+            {
+                message: `Unable to delete user: ${currentUserId} from Users collection. Reason: ${err}`
+            }
+        );
+    })
 
-
+    return res.status(200).json(
+        {
+            message: `Deleted user ${currentUser}`
+        }
+        );
 })
 
 router.post("/semester/student", async (req, res) => {
@@ -40,6 +66,10 @@ router.post("/semester/student", async (req, res) => {
             submission: req.body,
         })
     }
+})
+
+router.post("/school/student", async (req, res) => {
+    const { student_id, semester_id } = req.body;
 })
 
 export default router;
